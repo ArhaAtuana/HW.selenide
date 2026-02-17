109c130262e50400c103c85b55adaa38b061bb63
Ksenia Krestsova <arhaatuana@gmail.com>
2026-02-18 01:26:55 +0200

    Problem #1

---
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
@@ -0,0 +1,46 @@
+package ru.netology.cardDelivery;
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class CardDeliveryTest {
+
+    public String generateDate(int days, String pattern) {
+
+        return  LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+
+    public void ShouldAcceptValidForm() {
+        Selenide.open("http://localhost:9999/");
+
+        String planningDate = generateDate(4,"dd.MM.yyyy");
+
+        SelenideElement form = $("form");
+        form.$(By.cssSelector("[data-test-id=city] .input__control")).setValue("Вологда");
+        form.$(By.cssSelector("[data-test-id=date] input")).press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        //String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue(planningDate);
+        form.$(By.cssSelector("[data-test-id=name] .input__control")).setValue("Анна Асафьева");
+        form.$(By.cssSelector("[data-test-id=phone] .input__control")).setValue("+79998887766");
+        form.$(By.cssSelector("[data-test-id=agreement]")).click();
+        $$("button").find(Condition.text ("Забронировать")).click();
+
+        $(Selectors.withText("Встреча успешно забронирована")).should(Condition.visible, Duration.ofSeconds(15));
+
+    }
+}
